24c2379b117fcf48a9716996580c7c33ac9a9af4
Victor Galhardo Vela <35960632+VictorVela@users.noreply.github.com>
2022-09-17 14:16:44 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from 'react-native'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+
+import { Routes } from './src/routes';
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontsLoade] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  return (
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoade ? <Routes /> : <Loading/>}
+
+    </Background>
+  );
+}
+
+
---
+++ src/components/DuoCard/index.tsx
@@ -0,0 +1,67 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+import { GameController } from 'phosphor-react-native';
+
+import { Duoinfo } from '../Duoinfo';
+
+import { styles } from './styles';
+
+import { THEME } from '../../theme';
+
+export interface DuoCardProps {
+  id: string,
+  hourEnd: string,
+  hourStart: string,
+  name: string,
+  useVoiceChannel: boolean,
+  weekDays: string[]
+  yearsPlaying: number,
+}
+
+interface Props {
+  data: DuoCardProps;
+  onConnect: () => void;
+}
+
+export function DuoCard({ data, onConnect }: Props) {
+  return (
+    <View style={styles.container}>
+      <Duoinfo
+        label="Nome"
+        value={data.name}
+      />
+
+      <Duoinfo
+        label="Tempo de jogo"
+        value={`${data.yearsPlaying} anos`}
+      />
+
+      <Duoinfo
+        label="Disponibilidade"
+        value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}
+      />
+
+
+      <Duoinfo
+        label="Chamada de áudio"
+        value={data.useVoiceChannel ? "Sim" : "Não"}
+        colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onConnect}
+      >
+        <GameController
+          color={THEME.COLORS.TEXT}
+          size={20}
+        />
+
+        <Text style={styles.buttonTitle}>
+          Conectar
+        </Text>
+
+      </TouchableOpacity>
+
+    </View>
+  );
+}
